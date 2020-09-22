59a27af950fc29fc9876738f3299a5d63b4f2db4
Phil M. Harner <philmh@vt.edu>
2020-09-22 16:11:54 -0400

    Renamed stuff to make more sense in the context of this project

--- data-collection/scrape-titles.js
+++ data-collection/scrape-titles.js
@@ -3,6 +3,7 @@ const axios = require('axios').default;
 const cheerio = require('cheerio');
 let data = { books: [] };
 async function updateTableOfContents(url) {
+    console.log(1)
     await axios.get(url)
         .then( (html) => {
             const $ = cheerio.load(html.data);
@@ -10,11 +11,16 @@ async function updateTableOfContents(url) {
             $('a.font-red-sunglo').each((i, elem) => {
                 const title = $(elem).text();
                 const homepage = $(elem).attr('href');
-                data.chapters.push({ title, homepage });
+                data.books.push({ title, homepage });
             });
             const stringified = JSON.stringify(data, null, 4);
             console.log('Starting Table of Contents file write');
             fs.writeFile('table-of-contents.json', stringified, (err) => err ? console.log('Could not write table of contents') : console.log('Table of Contents written to file.'));
         });
+    console.log(2);
 }
 updateTableOfContents('https://www.royalroad.com/fictions/search?page=2&advanced=True');
+
+for (let i = 0; i < 5; i++) {
+    
+}
