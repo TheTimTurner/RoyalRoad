ccf14d40ba3bec9b41016b4fa68328a5b64aafd1
TheTimTurner <71408843+TheTimTurner@users.noreply.github.com>
2020-09-24 15:13:59 -0400

    fixed how we read in the array. we nw have a 1d array of titles

--- data-collection/JsonLoader.py
+++ data-collection/JsonLoader.py
@@ -2,9 +2,18 @@ import json
 import numpy as np
 
 def load_tester(path):
+    new_Title_Array=[]
+
     with open(path,'r',encoding='utf-8') as f:
         data = json.load(f)
+
     print(data)
-    return np.asarray(data)
+    print("-------")
+    
+    for i in range(len(data['books'])):
+        print(data['books'][i])
+        new_Title_Array.append(data['books'][i]['title'])
+        print(data['books'][i]['title'])
+    return np.asarray(new_Title_Array)
 
 np.save('table-of-contents.npy',load_tester('table-of-contents.json'))
